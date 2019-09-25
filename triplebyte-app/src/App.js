1ae4f074a82b7175885dd2f0eb71094a65b282cb
cd C:\RailsInstaller\Ruby2.2.0\bin <kostia.arlouski@gmail.com>
2019-09-25 09:53:03 -0400

    Add sass and css module support via sass package

--- triplebyte-app/src/App.js
+++ triplebyte-app/src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img src={logo} className={styles.Logo} alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
